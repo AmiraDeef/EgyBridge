50303796788bc33b40e7009923428d129fec064d
AmiraDeef <amiradeef4@gmail.com>
2026-09-08 22:06:33 +0300

    editing front server to cors

--- BE/app.js
+++ BE/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://egirises.vercel.app/",
+    origin: "https://egirises.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
