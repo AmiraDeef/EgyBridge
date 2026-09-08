61c0849e81b75cb919a08c71faa6de46d13caa60
AmiraDeef <amiradeef4@gmail.com>
2026-09-08 21:50:07 +0300

    adding front server to cors

--- BE/app.js
+++ BE/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://egirises.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
